721b8276eb8601fd7d68dcb57c40ea4b1a7b9ae3
SCY <shen421226@gmail.com>
2023-04-30 17:44:11 +0800

    npm run build 產生vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/wildlife-save/dist/'
-})
-
 module.exports = {
-  configureWebpack: config => {
-    config.performance = {
-      maxEntrypointSize: 10000000,
-      maxAssetSize: 40000000
-    }
-  },
-
   publicPath: '/wildlife-save/dist/'
 }
